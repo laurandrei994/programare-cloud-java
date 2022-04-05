74645635d23cf1f92815996b92ec5efc10754f51
laurandrei994 <65813936+laurandrei994@users.noreply.github.com>
2022-04-05 14:55:23 +0300

    Eureka solved

--- eureka/persons-server/src/main/java/com/eureka/persons/PersonsController.java
+++ eureka/persons-server/src/main/java/com/eureka/persons/PersonsController.java
@@ -1,50 +1,16 @@
-/*
-Freeware License, some rights reserved
-
-Copyright (c) 2019 Iuliana Cosmina
-
-Permission is hereby granted, free of charge, to anyone obtaining a copy 
-of this software and associated documentation files (the "Software"), 
-to work with the Software within the limits of freeware distribution and fair use. 
-This includes the rights to use, copy, and modify the Software for personal use. 
-Users are also allowed and encouraged to submit corrections and modifications 
-to the Software for the benefit of other users.
-
-It is not allowed to reuse,  modify, or redistribute the Software for 
-commercial use in any way, or for a user's educational materials such as books 
-or blog articles without prior permission from the copyright holder. 
-
-The above copyright notice and this permission notice need to be included 
-in all copies or substantial portions of the software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS OR APRESS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
 package com.eureka.persons;
 
+import com.eureka.persons.base.AbstractEntity;
 import com.eureka.persons.ex.NotFoundException;
 import com.eureka.persons.person.Person;
 import com.eureka.persons.services.PersonService;
-import com.eureka.persons.util.NumberGenerator;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriTemplate;
 
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Optional;
-
-import static com.eureka.persons.base.AbstractEntity.COMPARATOR_BY_ID;
 
 @RestController
 @RequestMapping("/persons")
@@ -58,54 +24,27 @@ public class PersonsController {
     /**
      * Handles requests to list all persons.
      */
+    //TODO find all persons using the functions already implemented and sort them by id
     @ResponseStatus(HttpStatus.OK)
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> list() {
         List<Person> persons = personService.findAll();
-        persons.sort(COMPARATOR_BY_ID);
+        persons.sort(AbstractEntity.COMPARATOR_BY_ID);
         return persons;
     }
 
     /**
      * Handles requests to create a person.
      */
+    //TODO save a person to the db or throw PersonsException
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public void create(@RequestBody Person person, BindingResult result, @Value("#{request.requestURL}")
-            StringBuffer originalUrl, HttpServletResponse response) {
-        if (result.hasErrors()) {
-            var errString = createErrorString(result);
-            throw new PersonsException(HttpStatus.BAD_REQUEST, "Cannot save entry because: " + errString);
-        }
-        if (StringUtils.isEmpty(person.getPassword())) {
-            person.setPassword(NumberGenerator.getPassword());
-        }
+    public void create(@RequestBody Person person, BindingResult result) {
         try {
-            var newPerson = personService.save(person);
-            response.setHeader("Location", getLocationForUser(originalUrl, newPerson.getId()));
+            Person createdPerson = personService.save(person);
         } catch (Exception e) {
-            throw new PersonsException(HttpStatus.UNPROCESSABLE_ENTITY, e);
+            throw new PersonsException(HttpStatus.CONFLICT, e.getMessage());
         }
-    }
-
-    /**
-     * Determines URL of user resource based on the full URL of the given request,
-     * appending the path info with the given childIdentifier using a UriTemplate.
-     */
-    static String getLocationForUser(StringBuffer url, Object childIdentifier) {
-        var template = new UriTemplate(url.toString() + "/{id}");
-        return template.expand(childIdentifier).toASCIIString();
-    }
-
-    private String createErrorString(BindingResult result) {
-        var sb = new StringBuilder();
-        result.getAllErrors().forEach(error -> {
-            if (error instanceof FieldError) {
-                FieldError err = (FieldError) error;
-                sb.append("Field '").append(err.getField()).append("' value error: ").append(err.getDefaultMessage()).append("\n");
-            }
-        });
-        return sb.toString();
     }
 
     /**
@@ -114,12 +53,14 @@ public class PersonsController {
      * @param id
      * @return
      */
+    //TODO find a person by id or throw NotFoundException
     @ResponseStatus(HttpStatus.OK)
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Person show(@PathVariable Long id) {
-        var personOpt = personService.findById(id);
-        if (personOpt.isPresent()) {
-            return personOpt.get();
+
+        Optional<Person> person = personService.findById(id);
+        if (person.isPresent()) {
+            return person.get();
         } else {
             throw new NotFoundException(Person.class, id);
         }
@@ -132,16 +73,17 @@ public class PersonsController {
      * @param id
      * @return
      */
+    //TODO update an existing person if found else throw NotFoundException
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
     public void update(@RequestBody Person updatedPerson, @PathVariable Long id) {
-        Optional<Person> personOpt = personService.findById(id);
-        if (personOpt.isPresent()) {
-            var person = personOpt.get();
-            person.setUsername(updatedPerson.getUsername());
-            person.setFirstName(updatedPerson.getFirstName());
-            person.setLastName(updatedPerson.getLastName());
-            personService.save(person);
+        Optional<Person> person = personService.findById(id);
+        if (person.isPresent()) {
+           Person existingPerson = person.get();
+           existingPerson.setFirstName(updatedPerson.getFirstName());
+           existingPerson.setUsername(updatedPerson.getUsername());
+           existingPerson.setLastName(updatedPerson.getLastName());
+           personService.save(existingPerson);
         } else {
             throw new NotFoundException(Person.class, id);
         }
@@ -152,10 +94,15 @@ public class PersonsController {
      *
      * @param id
      */
+    //TODO delete a person
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
-        var personOpt = personService.findById(id);
-        personOpt.ifPresent(value -> personService.delete(value));
+        Optional<Person> person = personService.findById(id);
+        if (person.isPresent()) {
+            personService.delete(person.get());
+        } else {
+            throw new NotFoundException(Person.class, id);
+        }
     }
 }
